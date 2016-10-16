b7fd1a72964c33568ca9416bca65f3ca36330ff1
rszeredi <riaszeredi@Imres-MacBook-Air.local>
2016-10-17 09:07:30 +1100

    Final commit before submission

--- proportion_optimal.py
+++ proportion_optimal.py
@@ -1,6 +1,139 @@
 import os
 from pprint import pprint
 
+
+def getNumbers(filename):
+
+	worse=0
+	same=0
+	better=0
+	totalInstances=0
+
+	worseGapSum =0
+
+	print filename
+
+	if os.path.isfile(filename):
+		print filename
+		for line in open(filename):
+			line = line.strip()
+			parts = line.split(", ")
+
+			gap=parts[8]
+			gap = gap.split(": ")
+			if gap[1] == '0':
+				gap=int(gap[1])
+			else:
+				gap=float(gap[1])
+
+			# get status
+			CP=parts[3]
+			CP = CP.split(": ")
+			status=CP[1]
+
+			# get ratio
+			ratioPart=parts[8]
+			ratioPart = ratioPart.split(": ")
+			ratio=float(ratioPart[1])
+
+			if status == 'Suboptimal':
+				totalInstances += 1
+				if gap > 0:
+					worse += 1
+					worseGapSum += gap
+				elif gap == 0:
+					same += 1
+				else:
+					better += 1
+
+	else:
+		print "Cannot find instance_count file."
+
+	if worse > 0:
+		averageWorseGap = worseGapSum/float(worse)
+	else:
+		averageWorseGap = None
+
+	return worse, same, better, averageWorseGap
+
+def getMaxGap(filename):
+
+	summ=0
+	totalInstances=0
+	maxGap=0
+
+	print filename
+
+	if os.path.isfile(filename):
+		print filename
+		for line in open(filename):
+			line = line.strip()
+			parts = line.split(", ")
+
+			gap=parts[8]
+			gap = gap.split(": ")
+			if gap[1] == '0':
+				gap=int(gap[1])
+			else:
+				gap=float(gap[1])
+
+			# get status
+			CP=parts[3]
+			CP = CP.split(": ")
+			status=CP[1]
+
+			# get ratio
+			ratioPart=parts[8]
+			ratioPart = ratioPart.split(": ")
+			ratio=float(ratioPart[1])
+
+			if status == 'Suboptimal' and gap > 0:
+				summ += gap
+				totalInstances += 1
+				if gap > maxGap:
+					maxGap = gap
+
+	else:
+		print "Cannot find instance_count file."
+
+	if totalInstances > 0:
+		ratioAverage = summ/totalInstances
+	else:
+		ratioAverage = None
+	return maxGap
+
+def getOptimalNumber(filename):
+
+	suboptimal = 0
+	optimal =0
+
+	if os.path.isfile(filename):
+		# print filename
+		for line in open(filename):
+			line = line.strip()
+			parts = line.split(", ")
+
+			gap=parts[8]
+			gap = gap.split(": ")
+			if gap[1] == '0':
+				gap=int(gap[1])
+			else:
+				gap=float(gap[1])
+
+			# get status
+			CP=parts[3]
+			CP = CP.split(": ")
+			status=CP[1]
+
+			if status == 'Suboptimal' or not status:
+				suboptimal += 1
+			elif status == 'Optimal':
+				optimal += 1
+
+	return optimal, suboptimal
+
+
+
 def getOptimalProportion(filename, measureBetterThanOptimal):
 
 	totalInstances=0
@@ -57,7 +190,7 @@ def getOptimalProportion(filename, measureBetterThanOptimal):
 	elif totalInstances > 0 and measureBetterThanOptimal:
 		proportionBetterThanOptimal = betterThanOptimalInstances/float(totalInstances)
 		proportionBetterThanOptimal = round(proportionBetterThanOptimal,2)*100
-		ratioAverage=round(summ/totalInstances,6)
+		ratioAverage=round((summ/totalInstances)*100,2)
 	elif not measureBetterThanOptimal:
 		proportionOptimal = None
 		ratioAverage = None
@@ -111,4 +244,82 @@ if __name__ == '__main__':
 				f.write('&')
 		f.write('\\\\ \n')
 
-	f.close()
+	# f.close()
+
+	# maxGap = {}
+	# for m in mList:
+	# 	for n in nList:
+	# 		filename = "%s/optimal_m%s_n%s.txt" %(SOURCE,m,n)
+
+	# 		maxGap[m,n] = getMaxGap(filename)
+
+	# pprint(maxGap)
+	# print max(maxGap.values())
+
+	# worse = {}
+	# same = {}
+	# better = {}
+	# full = {}
+	# averageWorseGap = {}
+	# for m in mList:
+	# 	for n in nList:
+	# 		filename = "%s/optimal_m%s_n%s.txt" %(SOURCE,m,n)
+	# 		full[n,m] = []
+
+	# 		worse[n,m], same[n,m], better[n,m], averageWorseGap[n,m] = getNumbers(filename)
+	# 		full[n,m].append(worse[n,m])
+	# 		full[n,m].append(same[n,m])
+	# 		full[n,m].append(better[n,m])
+
+
+	# print "WORSE"
+	# pprint(worse)
+	
+	# # print "SAME"
+	# # pprint(same)
+	# # print "BETTER"
+	# # pprint(better)
+	# print 'worse gap'
+	# pprint(averageWorseGap)
+
+	# print sum(worse.values())
+	# print sum(same.values())
+	# print sum(better.values())
+
+	# # print "FULL"
+	# # pprint(full)
+	# # print sum([full.values()[i] for i)
+
+	# for key in averageWorseGap.keys():
+	# 	if not averageWorseGap[key]:
+	# 		averageWorseGap.pop(key, None)
+
+	# pprint(averageWorseGap)
+	# print sum(averageWorseGap.values())/len(averageWorseGap.keys())
+
+	# get optimal 
+
+	# optimal = {}
+	# suboptimal = {}
+	# full = {}
+
+
+	# for m in mList:
+	# 	for n in nList:
+	# 		filename = "%s/optimal_m%s_n%s.txt" %(SOURCE,m,n)
+	# 		full[n,m] = []
+
+	# 		optimal[n,m], suboptimal[n,m] = getOptimalNumber(filename)
+	# 		full[n,m].append(optimal[n,m])
+	# 		full[n,m].append(suboptimal[n,m])
+
+	# 		# print full[n,m],
+	# 		if optimal[n,m] + suboptimal[n,m] >0:
+	# 			proportionOptimal = optimal[n,m]/float(optimal[n,m] + suboptimal[n,m])
+	# 		else:
+	# 		 proportionOptimal = None
+	# 		# print full[n,m],
+	# 		print proportionOptimal,
+	# 	print ''
+
+	# pprint(full)
